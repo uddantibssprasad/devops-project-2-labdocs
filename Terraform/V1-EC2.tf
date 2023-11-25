e5e13d01d3d6ffc1d683204e065763c5117d4e81
Prasad U <uddantibssprasad@gmail.com>
2023-11-25 13:46:44 +0530

    modified V1 EC2

--- Terraform/V1-EC2.tf
+++ Terraform/V1-EC2.tf
@@ -1,11 +1,12 @@
 # Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "Ansible" {
-  ami           = ami-0230bd60aa48260c6
+  ami           = "ami-0230bd60aa48260c6"
   instance_type = "t3.micro"
+  key_name      = "dp2"
 
   tags = {
     Name = "project2"
